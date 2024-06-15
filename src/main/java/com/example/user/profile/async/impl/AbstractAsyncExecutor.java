145b18ecc9908863c50fec6f7ccc290f844e7876
Satyajit <thatssatya@gmail.com>
2024-06-15 23:42:22 +0530

    added async executor

---
+++ src/main/java/com/example/user/profile/async/impl/AbstractAsyncExecutor.java
@@ -0,0 +1,25 @@
+package com.example.user.profile.async.impl;
+
+import com.example.user.profile.async.AsyncExecutor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Supplier;
+
+@RequiredArgsConstructor
+@Slf4j
+public class AbstractAsyncExecutor implements AsyncExecutor {
+    private final ExecutorService executorService;
+
+    @Override
+    public <T> CompletableFuture<T> supply(Supplier<T> supplier) {
+        return CompletableFuture.supplyAsync(supplier, executorService);
+    }
+
+    @Override
+    public <T> void run(Supplier<T> supplier) {
+        CompletableFuture.runAsync(supplier::get);
+    }
+}
